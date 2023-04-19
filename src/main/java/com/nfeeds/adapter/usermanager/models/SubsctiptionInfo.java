8f5c007f18823f4c7fc1634db2ed46abf99e894a
Leonardo Malcotti <leonardo.malcotti@studenti.unitn.it>
2023-04-19 16:44:30 +0200

    added record for subscription infos

---
+++ src/main/java/com/nfeeds/adapter/usermanager/models/SubsctiptionInfo.java
@@ -0,0 +1,15 @@
+package com.nfeeds.adapter.usermanager.models;
+
+/**
+ * <p> Record representing an entry of a Subscription in the database. </p>
+ * <p> To marshall a String to this record use: </p>
+ * <pre>
+ * {@code
+ * var sub_info = new ObjectMapper().readValue( jsonString , SubsctiptionInfo.class);
+ * }
+ * </pre>
+ * @param userId The unique id of the user that has this subscription.
+ * @param topicId The topic to which the user is subscribed to.
+ * @param callback The endpoint to which the system will send messages to.
+ */
+public record SubsctiptionInfo(String userId, String topicId, String callback) {}
